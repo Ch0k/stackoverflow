7260a0d164ea3b00d8f9819ee65bf5a11b4ea445
andreevsergey <andreev,sv89@yandex.ru>
2021-10-27 10:02:39 +0300

    add Question model with rspec

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  it {should validate_presence_of :title}
+  it {should validate_presence_of :body}
+  it 'validates presense of title' do
+    expect(Question.new(body: '123')).to_not be_valid 
+  end
+  it 'validates presense of body' do
+    expect(Question.new(title: '123')).to_not be_valid 
+  end
+end
