977e996033eea034059f0efeb59a1e3ccfe46607
andreevsergey <andreev,sv89@yandex.ru>
2021-10-29 00:12:19 +0300

    add model Answer

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it {should validate_presence_of :title}
-  it {should validate_presence_of :body}
-  it 'validates presense of title' do
-    expect(Question.new(body: '123')).to_not be_valid 
-  end
-  it 'validates presense of body' do
-    expect(Question.new(title: '123')).to_not be_valid 
-  end
+  it {  should validate_presence_of :title  }
+  it {  should validate_presence_of :body  }
+  it {  should have_many(:answers).dependent(:destroy)  }
 end
