5cabdd2cfe1d3353299cfdee73ffa374d7086766
andreevsergey <andreev,sv89@yandex.ru>
2021-11-24 21:55:29 +0300

    add create new anser fith files with spec

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -41,15 +41,18 @@ feature 'User can create answer', %q{
       expect(page).to have_content 'error'
     end
 
-    scenario 'asks a question with attached file',js: true do
+    scenario 'asks a question with attached file', js: true do
+      
       within '.new_answer' do
-        fill_in 'Body', with: 'text text text'
+        fill_in 'Body', with: 'new answer answer answer'
         attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Create answer'
       end
-      click_on 'Create answer'
-
-      expect(page).to have_link 'rails_helper.rb'
-      expect(page).to have_link 'spec_helper.rb'
+      save_and_open_page
+      within '.answers' do
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
     end
   end
 
