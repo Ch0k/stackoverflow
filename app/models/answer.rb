ee698bf3a50091936fbda62396e9112d02dce726
andreevsergey <andreev,sv89@yandex.ru>
2021-12-29 23:54:20 +0300

    add concern has_vote and has_unvote

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,22 +1,14 @@
 class Answer < ApplicationRecord
+  include HasVote
+  include HasUnvote
+
   belongs_to :question
   belongs_to :user
   has_many :links, dependent: :destroy, as: :linkable
-  has_many :votes, dependent: :destroy, as: :votable
-  has_many :unvotes, dependent: :destroy, as: :unvotable
-
 
   has_many_attached :files
 
   accepts_nested_attributes_for :links, reject_if: :all_blank
 
   validates :body, presence: true, length: { minimum: 15 }
-
-  def voted?(user)
-    self.votes.where(user_id: user.id).count >= 1
-  end
-
-  def unvoted?(user)
-    self.unvotes.where(user_id: user.id).count >= 1
-  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,11 @@
 class Question < ApplicationRecord
+  include HasVote
+  include HasUnvote
+  
   has_many :answers, dependent: :destroy
   belongs_to :user
   belongs_to :best_answer, class_name: 'Answer', optional: true
   has_many :links, dependent: :destroy, as: :linkable
-  has_many :votes, dependent: :destroy, as: :votable
-  has_many :unvotes, dependent: :destroy, as: :unvotable
 
   has_one :badge
 
@@ -21,13 +22,5 @@ class Question < ApplicationRecord
     if self.badge.present?
       self.best_answer.user.badges << self.badge
     end
-  end
-
-  def voted?(user)
-    self.votes.where(user_id: user.id).count >= 1
-  end
-
-  def unvoted?(user)
-    self.unvotes.where(user_id: user.id).count >= 1
   end
 end
