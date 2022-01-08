e3b2c82e4a6dd2bad21fc73dbaa41d7086bbae77
andreevsergey <andreev,sv89@yandex.ru>
2022-01-08 14:36:53 +0300

    add partial vote and add vote to answer

--- app/controllers/concerns/voted.rb
+++ app/controllers/concerns/voted.rb
@@ -5,61 +5,38 @@ module Voted
   end
 
   def vote
-    #if voted?(user)
-    #else
-      Vote.create!(votable_type: model_klass, votable_id: @votable.id, user_id: current_user.id)
-      respond_to do |format|
-        format.json { render json: {
-                                      id: @votable.id,
-                                      votes: @votable.votes.count,
-                                      rating: @votable.votes.count - @votable.unvotes.count
-                                    } 
-                                  }
-      end
-    #end
+    Vote.create!(votable_type: model_klass, votable_id: @votable.id, user_id: current_user.id)
+    render_json
   end
 
   def delete_vote
     @votable.votes.where(user_id: current_user.id).destroy_all
-    respond_to do |format|
-      format.json { render json: {
-                                    id: @votable.id,
-                                    votes: @votable.votes.count,
-                                    rating: @votable.votes.count - @votable.unvotes.count
-                                  } 
-        }
-    end
+    render_json
   end
 
   def unvote
     Unvote.create!(unvotable_type: model_klass, unvotable_id: @votable.id, user_id: current_user.id)
-    respond_to do |format|
-      format.json { render json: { 
-                                    id: @votable.id,
-                                    votes: @votable.votes.count,
-                                    unvotes: @votable.unvotes.count,
-                                    rating: @votable.votes.count - @votable.unvotes.count
-
-        }
-      }
-    end
+    render_json
   end
 
   def delete_unvote
     @votable.unvotes.where(user_id: current_user.id).destroy_all
+    render_json
+  end
+
+  private 
+
+  def render_json
     respond_to do |format|
       format.json { render json: {
                                     id: @votable.id,
                                     votes: @votable.votes.count,
                                     unvotes: @votable.unvotes.count,
                                     rating: @votable.votes.count - @votable.unvotes.count
-
-        }
+                                  }
       }
     end
   end
-
-  private 
   
   def model_klass
     controller_name.classify.constantize
